2cee2b612d6888a225934b019c232536c606a2c2
distantcove@gmail.com <distantcove@gmail.com>
2016-02-21 22:12:18 -0500

    changed image-url to image_url

--- db/migrate/20160219001458_create_products.rb
+++ db/migrate/20160219001458_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.string :image-url
+      t.string :image_url
 
       t.timestamps null: false
     end
