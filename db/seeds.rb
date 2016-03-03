7eaa31dd0b0703650ce5d7a1c12f9d5e897ae5a9
distantcove@gmail.com <distantcove@gmail.com>
2016-03-03 07:54:44 -0500

    added products via seeds.rb and attempted to link products page from home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+
+Product.create(:image_url => "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwilk-O4gaLLAhWF8j4KHSakBRwQjRwIBw&url=http%3A%2F%2Fwww.britannica.com%2Ftechnology%2Fpenny-farthing&psig=AFQjCNEvwHjw3f0hCfRYRSMeIqaD9cCkRw&ust=1457008404469383", :name => "seed-generated product", :description => "I also created this product without using the HTML form!")
