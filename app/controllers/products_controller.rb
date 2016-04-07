049d2c885f35d107695ab612a43c14fa155ca170
distantcove@gmail.com <distantcove@gmail.com>
2016-04-07 07:33:56 -0400

    re-routed root to landing_page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
 
   # GET /products/1/edit
   def edit
-    @product= Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
   # POST /products
