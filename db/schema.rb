56c60a8faf60fa59b45bb9038562590c5c321c7d
distantcove@gmail.com <distantcove@gmail.com>
2016-05-06 08:12:22 -0400

    had to reinstall devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420120114) do
+ActiveRecord::Schema.define(version: 20160505152455) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -33,10 +33,6 @@ ActiveRecord::Schema.define(version: 20160420120114) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -47,6 +43,8 @@ ActiveRecord::Schema.define(version: 20160420120114) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
